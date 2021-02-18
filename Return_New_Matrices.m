52dff86ec53c868b07d81edc8f1f6a84a703d16b
EdMHill <>
2021-02-18 01:34:02 +0000

    Comments added to npi_change.m & Return_New_Matrices.m

--- Return_New_Matrices.m
+++ Return_New_Matrices.m
@@ -1,11 +1,31 @@
 function [new_UK_from_toH, new_UK_from_toW, new_UK_from_toS, new_UK_from_toO] = Return_New_Matrices(SCHOOL_CLOSURES, WORK_CLOSURES, SOCIAL_DISTANCING, Compliance, UK_from_toH, UK_from_toW, UK_from_toS, UK_from_toO)
+%% function to amend the mixing matrices
 
-% order is H, W, S , O
+% Inputs:
+% SCHOOL_CLOSURES, WORK_CLOSURES - Maximum proportional reduction in
+%                                       contacts in school & work settings.
+% SOCIAL_DISTANCING - Maximum proportional reduction in contacts in other settings
+% Compliance - Adherence to NPIs
+% UK_from_toH, UK_from_toW, UK_from_toS, UK_from_toO - 
+%   The original mixing matrices/contact arrays.
+%   Order is Household, Work, School, Other
 
+% Outputs:
+% new_UK_from_toH, new_UK_from_toW, new_UK_from_toS, new_UK_from_toO -
+%   The amended mixing matrices/contact arrays.
+%   Order is Household, Work, School, Other
+
+% Reshape Compliance input if needed
+% Four age groupings: PreSchool, School, Adult, Elderly. 
 if length(Compliance)==1
     Compliance=[1 1 1 1]'*Compliance;
 end
 
+% Map four age groupings to the five-year age bands
+% Preschool: 0-4yrs
+% School: 5-19yrs
+% Adult: 20-69yrs
+% Elderly: 70-100+yrs
 if length(Compliance)==4
     Compliance=Compliance([1 2 2 2 3 3 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4]);
 end
@@ -15,27 +35,34 @@ if size(Compliance,2)>1
 end
 
 % Raw Uncontrolled Scaling
+% Indexes: [Household, Work, School, Other]
 ReductionPreSchool=[1.0 1.0 1.0 1.0];
 ReductionSchool=[1.0 1.0 1.0 1.0];
 ReductionAdult=[1.0 1.0 1.0 1.0];   workers_interacting_with_other=0.3;
 ReductionElderly=[1.0 1.0 1.0 1.0];
 hhq=0;
-
 txt=[];
+
+% Maximum impact of school closures on each of the four age groupings
 if SCHOOL_CLOSURES
     ReductionPreSchool(3)=1-SCHOOL_CLOSURES;  ReductionSchool(3)=1-SCHOOL_CLOSURES; ReductionAdult(3)=1-SCHOOL_CLOSURES; ReductionElderly(3)=1-SCHOOL_CLOSURES;
-    ReductionSchool(1)=1+0.25*SCHOOL_CLOSURES;
+    ReductionSchool(1)=1+0.25*SCHOOL_CLOSURES; % Home contacts increase for school children
     txt='SC';
 end
 
+% Maximum impact of work closures on each of the four age groupings
 if WORK_CLOSURES
     ReductionPreSchool(2)=1-WORK_CLOSURES;  ReductionSchool(2)=1-WORK_CLOSURES; ReductionAdult(2)=1-WORK_CLOSURES; ReductionElderly(2)=1-WORK_CLOSURES;
-    ReductionAdult(1)=1+0.25*WORK_CLOSURES;
+    ReductionAdult(1)=1+0.25*WORK_CLOSURES; % Home contacts increase for adults
     if isempty(txt)  txt='WC'; else txt=[txt '+WC']; end
 end
 
+% Maximum impact of social distancing/reduction of contacts in other settings 
+% on each of the four age groupings
 if SOCIAL_DISTANCING
     ReductionPreSchool(4)=1-SOCIAL_DISTANCING;  ReductionSchool(4)=1-SOCIAL_DISTANCING; ReductionAdult(4)=1-SOCIAL_DISTANCING; ReductionElderly(4)=1-SOCIAL_DISTANCING;
+    
+    % Home contacts increase in all age age groups
     ReductionPreSchool(1)=1+0.25*SOCIAL_DISTANCING;  ReductionSchool(1)=ReductionSchool(1)+0.25*SOCIAL_DISTANCING; ReductionAdult(1)=ReductionAdult(1)+0.25*SOCIAL_DISTANCING; ReductionElderly(1)=1.0+0.25*SOCIAL_DISTANCING;
     if isempty(txt)  txt='SD'; else txt=[txt '+SD']; end
 end
@@ -43,13 +70,14 @@ end
 
 if isempty(txt)  txt='None'; end
 
-Run_Name=txt;
-
+% Get maximum reduction scale factors, w.r.t adherence values.
 ReductionPreSchool=ReductionPreSchool'*Compliance(1)' + [1 1 1 1]'*(1-Compliance(1));
 ReductionSchool=ReductionSchool'*Compliance(2:4)' + [1 1 1 1]'*(1-Compliance(2:4)');
 ReductionAdult=ReductionAdult'*Compliance(5:14)' + [1 1 1 1]'*(1-Compliance(5:14)');
 ReductionElderly=ReductionElderly'*Compliance(15:21)' + [1 1 1 1]'*(1-Compliance(15:21)');
 OtherVector=[ReductionPreSchool(4) ReductionSchool(4,:) ReductionAdult(4,:) ReductionElderly(4,:)];
+
+% Iterate over each five-year age band and populate output mixing matrices
 
 f=1; %pre-school
 new_UK_from_toH(f,:)=UK_from_toH(f,:)*ReductionPreSchool(1);
--- npi_change.m
+++ npi_change.m
@@ -1,5 +1,28 @@
 function [aC,new_UK_from_toH, new_UK_from_toW, new_UK_from_toS, new_UK_from_toO] = npi_change(S_New_Var,Impact_of_New_Var,W,T,Region,ComplianceT,Region_PP,PD_Lockdown,Run_stop, UK_from_toH, UK_from_toW, UK_from_toS, UK_from_toO)
+%% function to amend the mixing matrices in response to 
+%% school openings & presence of more transmissible variant
 
+% Inputs:
+% S_New_Var - Fraction of virus in circulation that is more transmissible variant
+% Impact_of_New_Var - Increase in transmissability due to new varaint
+% W - Current index for accessing adherence array
+% T - Time
+% Region - ID for current region being analysed
+% ComplianceT - Adherence to NPIs
+% Region_PP - Population per five-year age group in the current region
+% Run_stop - Time at which this phase of NPI strength ends.
+% UK_from_toH, UK_from_toW, UK_from_toS, UK_from_toO - 
+%   The original mixing matrices/contact arrays.
+%   Order is Household, Work, School, Other
+
+% Outputs:
+% aC - Amended adherence values
+% new_UK_from_toH, new_UK_from_toW, new_UK_from_toS, new_UK_from_toO -
+%   The amended mixing matrices/contact arrays.
+%   Order is Household, Work, School, Other
+
+
+% Initialise amended adherence vector (aC)
 mmC=max(max(abs(ComplianceT)));
 if mmC>0
     if length(ComplianceT(:,W))==1
@@ -14,6 +37,7 @@ if mmC>0
     
 end
 
+% Based on time T, specify if schools are open by binary variable InSchoolFlag
 InSchoolFlag=0;
 if Region==10
     if T(end)>=230  % ie Mid-August put schools back in Scotland
@@ -49,7 +73,7 @@ if T(end)>=458 && T(end)<472
     InSchoolFlag=0; % Easter
 end
 
-
+% If schools are open, amend School age groups aC (age groups 2-4, so 5-19yrs)
 if InSchoolFlag & ComplianceT(1,W)>=0
     aC(2:4)=0.2*aC(2:4);
 end
@@ -57,11 +81,13 @@ end
 
 PD_Lockdown=PD_Lockdown+(Run_stop(W)-T(end)).*(Region_PP(Region,:)*aC)./mmC;
 
+% Get revised mixing matrices
 [new_UK_from_toH, new_UK_from_toW, new_UK_from_toS, new_UK_from_toO] = Return_New_Matrices(0.95, 0.8, 0.95, aC, UK_from_toH, UK_from_toW, UK_from_toS, UK_from_toO);
 
-
+% Compute scaling factor due to new variant
 Multi_Factor = 1 + S_New_Var(T(end)) * Impact_of_New_Var;  % where T(end) should be the current time.
 
+% Scale contacts according to transmissability scaling factor of variant
 new_UK_from_toH = new_UK_from_toH * Multi_Factor;
 new_UK_from_toW = new_UK_from_toW * Multi_Factor;
 new_UK_from_toS = new_UK_from_toS * Multi_Factor;
